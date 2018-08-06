3c67d7be730c507c95cc45d1c8d201c51b2dae18
Mei Xianming <meixm@jxgtmbjt.com>
2018-08-06 23:58:26 +0800

    获取banner数据

---
+++ src/api/index.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import qs from 'qs';
+
+axios.defaults.baseURL = window.location.hostname === 'https://www.baidu.com/' ? 'http://120.79.162.149:3000' : 'http://120.79.162.149:3000';
+export const Request = (payload) => {
+  return axios.get(payload.url, {
+      params: payload.data
+    })
+    .then(response => {
+      if (respponse.status === 200) {
+        return response.data;
+      }
+    })
+    .catch(error => {
+      return error;
+    })
+}
+export const getBanner = () => {
+  return Request({
+    url: "/banner"
+  })
+}
--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -8,20 +8,40 @@
 <template>
   <div class="recommend">
     <mei-header></mei-header>
-    <div class="test"> mei</div>
+    <swiper :list="testList"
+            :auto="true"></swiper>
   </div>
 </template>
 <script>
 import MeiHeader from "@/components/header"
+import {Swiper} from "vux"
+import {getBanner} from '@/api/index'
 export default {
   name: "recommend",
   components:{
-    MeiHeader
+    MeiHeader,
+    Swiper
   },
   data(){
     return {
-
+      testList:[
+        {
+          url: 'javascript:',
+          img: 'https://static.vux.li/demo/1.jpg',
+          title: '送你一朵fua'
+        },
+        {
+          url: 'javascript:',
+          img: 'https://static.vux.li/demo/5.jpg',
+          title: '送你一次旅行',
+          fallbackImg: 'https://static.vux.li/demo/3.jpg'
+        }
+      ]
     }
+  },
+  mounted(){
+    const a = getBanner();
+    console.log(a);
   }
 }
 </script>
